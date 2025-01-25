c34f9c7d3fa43050f4461ab6de8c8431f696fa3a
HendersonDaniel <danielhenderson.codeprojects@gmail.com>
2025-01-25 04:30:07 +0000

    create rust project

---
+++ my_project/target/debug/deps/my_project-16f9cc0a7824f1a5.d
@@ -0,0 +1,5 @@
+/workspaces/COMP423-rust-project/my_project/target/debug/deps/my_project-16f9cc0a7824f1a5: src/main.rs
+
+/workspaces/COMP423-rust-project/my_project/target/debug/deps/my_project-16f9cc0a7824f1a5.d: src/main.rs
+
+src/main.rs:
---
+++ my_project/target/debug/my_project.d
@@ -0,0 +1,1 @@
+/workspaces/COMP423-rust-project/my_project/target/debug/my_project: /workspaces/COMP423-rust-project/my_project/src/main.rs
